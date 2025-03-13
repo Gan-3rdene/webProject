43acb87efced10e29c83248c85137d8ab85778a4
Gan-3rdene <ganerdenegansukh8@gmail.com>
2025-03-13 20:56:35 +0800

    AAAAAAAAAAAAAAAAAAA

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/webProject',
+    assetPrefix: '/webProject',
 };
 
 export default nextConfig;
